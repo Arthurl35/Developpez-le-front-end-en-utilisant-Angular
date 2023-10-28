1529f23bcea7dd87eb929e31e7c2c46b109d9d86
arthur levesque <arthurlevesque35@gmail.com>
2023-10-28 23:03:14 +0200

    display none sur la legende créée par défaut de chartJs, boucler sur le nombre de pays afin d'ajouter une légende en fonction des parts du camembert

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Chart } from 'chart.js/auto';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Observable } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Chart } from 'chart.js/auto';
+import annotationPlugin from 'chartjs-plugin-annotation';
+import datalabelsPlugin from 'chartjs-plugin-datalabels';
+
+
+Chart.register(annotationPlugin);
+Chart.register(datalabelsPlugin);
+
+// Importez les types d'annotation spécifiques du plugin
+
+
 
 @Component({
   selector: 'app-chart',
@@ -29,8 +39,36 @@ export class ChartComponent implements OnInit {
     });
   }
 
+
   createChart() {
     const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const totalMedals = this.medalsCount.reduce((a, b) => a + b, 0); // Total des médailles
+
+    const annotations: Record<string, any> = {}; // Définissez un type pour les annotations
+
+    for (let i = 0; i < this.countries.length; i++) {
+      const label = `label${i + 1}`;
+      const xValue = (this.medalsCount[i] / totalMedals) * 100; // Position en pourcentage
+      const yValue = 50; // Vous pouvez ajuster la valeur y si nécessaire
+
+      annotations[label] = {
+        type: 'label',
+        xValue: xValue,
+        yValue: yValue,
+        xAdjust: 0, // Ajustez si nécessaire
+        yAdjust: 0, // Ajustez si nécessaire
+        backgroundColor: 'rgba(245,245,245)',
+        content: [`In this point of time, ${this.countries[i]}`, 'something happened'],
+        textAlign: 'start',
+        font: {
+          size: 18
+        },
+        callout: {
+          display: true,
+          side: 10
+        }
+      };
+    }
 
     new Chart(ctx, {
       type: 'pie',
@@ -44,7 +82,28 @@ export class ChartComponent implements OnInit {
             // Ajoutez d'autres couleurs si nécessaire
           ]
         }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+          annotation: {
+            annotations: annotations // Utilisation de l'objet annotations dynamique
+          }
+        }
       }
     });
   }
+
+
+
+
+
+
+
+
+
+
+
 }
