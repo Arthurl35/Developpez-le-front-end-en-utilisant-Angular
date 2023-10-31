4d38dbc265e80e3a69929afa7643658e487f6aea
arthur levesque <arthurlevesque35@gmail.com>
2023-10-31 10:26:41 +0100

    ajouter de la route pour la page détail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ChartListComponent } from './chart-list/chart-list.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'chart-list/:id',
+    component: ChartListComponent ,
   },
   {
     path: '**', // wildcard
--- src/app/chart-list/chart-list.component.ts
+++ src/app/chart-list/chart-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Olympic } from '../core/models/Olympic';
 
 @Component({
   selector: 'app-chart-list',
   templateUrl: './chart-list.component.html',
   styleUrls: ['./chart-list.component.scss']
 })
-export class ChartListComponent {
+export class ChartListComponent implements OnInit {
+  @Input() selectedChartData!: Olympic;
+  totalParticipations: number = 0;
+  totalMedals: number = 0;
+  totalAthletes: number = 0;
 
+  ngOnInit() {
+    if (this.selectedChartData) {
+      // Calcul du nombre total de participations, médailles et athlètes
+      this.selectedChartData.participations.forEach(participation => {
+        this.totalParticipations++;
+        this.totalMedals += participation.medalsCount;
+        this.totalAthletes += participation.athleteCount;
+      });
+    }
+  }
 }
