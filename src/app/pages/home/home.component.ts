6e6b5b819f7aa62c836a1fbf15a2ee4be35af5a9
arthur levesque <arthurlevesque35@gmail.com>
2023-11-17 16:40:05 +0100

    Bonne pratique | Assurer encore la fuite de mémoire si le composant est détruit, unsubscribe les deux Observables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { CustomContentItem } from 'src/app/core/models/CustomContentItem';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
@@ -10,10 +9,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$!: Observable<Olympic[] | null>;
   public numberJOs$!: Observable<number | null>;
   public homeCustomContent: CustomContentItem[] = [];
+
+  private olympicsSubscription!: Subscription;
+  private numberJOsSubscription!: Subscription;
 
   constructor(private olympicService: OlympicService) {}
 
@@ -21,18 +23,26 @@ export class HomeComponent implements OnInit {
     this.olympics$ = this.olympicService.getOlympics();
     this.numberJOs$ = this.olympicService.getNumberJo();
 
-    combineLatest([this.numberJOs$, this.olympics$]).pipe(
-      map(([numberJOs, olympics]) => {
-        if (numberJOs !== null && olympics !== null) {
-          return [
-            { label: 'Number of JOs', value: numberJOs },
-            { label: 'Number of countries', value: olympics.length },
-          ];
-        }
-        return [];
-      })
-    ).subscribe((content) => {
-      this.homeCustomContent = content;
+    this.numberJOsSubscription = this.numberJOs$.subscribe((numberJOs) => {
+      if (numberJOs !== null) {
+        this.numberJOsSubscription?.unsubscribe(); // Désabonnement après récupération de la valeur
+
+        this.olympicsSubscription = this.olympics$.subscribe((olympics) => {
+          if (olympics !== null) {
+            this.olympicsSubscription?.unsubscribe(); // Désabonnement après récupération de la valeur
+
+            this.homeCustomContent = [
+              { label: 'Number of JOs', value: numberJOs },
+              { label: 'Number of countries', value: olympics.length },
+            ];
+          }
+        });
+      }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.olympicsSubscription.unsubscribe();
+    this.numberJOsSubscription.unsubscribe();
   }
 }
